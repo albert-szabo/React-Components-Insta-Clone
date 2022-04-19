c1d9b209cb19be57b4fb5900aad25e7e503c5665
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-19 15:28:50 -0700

    Added like post functionality

--- src/App.js
+++ src/App.js
@@ -32,6 +32,14 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedPosts = posts.map(post => {
+      if (postId === post.id) {
+        return { ...post, likes: post.likes + 1 };
+      } else {
+        return post;
+      }
+    })
+    setPosts(updatedPosts);
   };
 
   return (
